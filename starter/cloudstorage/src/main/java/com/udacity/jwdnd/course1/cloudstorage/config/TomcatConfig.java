94884dcf7ff71f1bbb7b4f698b76d6d65f370c33
yassine01 <yassine.mjardi@gmail.com>
2024-08-04 14:32:16 +0200

    :fire: Adding custom tomcat limit size

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/TomcatConfig.java
@@ -0,0 +1,19 @@
+package com.udacity.jwdnd.course1.cloudstorage.config;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfig {
+
+    @Bean
+    public ConfigurableServletWebServerFactory webServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers(connector -> {
+            connector.setMaxPostSize(10485760); // 10 MB
+        });
+        return factory;
+    }
+}
